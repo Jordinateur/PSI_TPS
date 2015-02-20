47891ffed701fc8962f8bc26bac4cfa97f73a6ce
Jordinateur <jordan.gaudron@gmail.com>
2015-02-20 10:20:46 +0100

    Amelioration des rapports d erreur sur les arguments. Amelioration du convertirrseur afin de le rendre plus clair

--- tp1_ex1.sh
+++ tp1_ex1.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-
 is_music_file() {
-	file -i $1 | grep -qi 'audio' || return 1
+	echo "$1"
+	file -i "$1" | grep -qi 'audio' || return 1
 }
 is_valid_date() {
 	grep -q '^\(2[0-3]\)\|\([0-1][0-9]\):[0-5][0-9]:[0-5][0-9]' $1 || return 2
 }
-if [ $# -ne 2 ]; then exit 3; fi
-if ! is_valid_date <<< $1; then exit 4; fi
-if ! is_music_file $2; then exit 5; fi
+if [ $# -ne 2 ]; then echo Invalid number of arguments; exit 3; fi
+if ! is_valid_date <<< $1; then echo Invalid Time;exit 4; fi
+if ! is_music_file "$2"; then echo Invalid File;exit 5; fi
 Diff=$(($(date -d $1 +%s) - $(date +%s)))
 if [ $Diff -lt 0 ]
 then 
@@ -17,6 +17,7 @@ then
 else 
 	Timer=$Diff;
 fi
+echo Reveil dans $Timer seconds
 sleep $Timer
 rhythmbox $2 &
 PID=$!
--- tp1_ex3.sh
+++ tp1_ex3.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-
-#wget "http://www.banque-france.fr/fileadmin/user_upload/banque_de_france/Economie_et_Statistiques/Changes_et_Taux/2.csv" > banque
-
-if [ $# -gt 3 ] | [ $# -lt 2 ]
+if [ $# -eq 4 ]
+	then
+	wget -O banque --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" --header="Referer: Hello" "http://www.banque-france.fr/fileadmin/user_upload/banque_de_france/Economie_et_Statistiques/Changes_et_Taux/2.csv"
+	clear
+	echo Fichier mis a jour
+fi
+if [ $# -eq 1 ] & [ $1 = "-v" ]
+	then
+	echo "Liste des monnaies"
+	echo $(head -n 8 banque | tail -n 1 | cut -d';' -f 2- | tr ';' '\n')
+fi
+if [ $# -gt 4 ] | [ $# -lt 2 ]
 then
 	exit 1 
-	echo
+fi
+if [ $1 = $3 ]
+	then
+	echo "$2 $(echo $1 | tr '[:lower:]' '[:upper:]') = $2 $(echo $1 | tr '[:lower:]' '[:upper:]')"
+	exit 0
 fi
 
 head -n 8 banque | tail -n 1 | tr ';' '\n' > pays
-
 tail -n 1 banque | tr ';' '\n' > money
-
 paste pays money > final
+echo Mise a jour du $(head -n 1 money)
+rm money
+rm pays
 
 
 pays=$(grep -i $1 final | cut -f2 | tr ',' '.')
@@ -22,19 +35,17 @@ if [ $# -eq 3 ]
 then
 pays_f=$(grep -i $3 final | cut -f2 | tr ',' '.')
 fi
-
+rm final
 valeur_int=$(bc -l <<< "$2/$pays")
-echo "$#"
 
 if [ $# -eq 2 ]
 then 
-	echo " $1 = $valeur_int eur "
+	echo "$2 $(echo $1 | tr '[:lower:]' '[:upper:]') = $(echo $valeur_int | sed 's/\([0-9]\{1,\}\)\.\([0-9][0-9]\)*/\1,\2/') EUR "
 	exit 0
 fi
 
 valeur_final=$(bc -l <<< "$valeur_int*$pays_f")
-
-echo " $1 = $valeur_final $3 "
+echo "$2 $(echo $1 | tr '[:lower:]' '[:upper:]') = $(echo $valeur_final | sed 's/\([0-9]\{1,\}\)\.\([0-9][0-9]\)*/\1,\2/') $(echo $3 | tr '[:lower:]' '[:upper:]') "
 
 exit 0
 
